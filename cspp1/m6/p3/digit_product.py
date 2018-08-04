6fc485a9dcdf3e14e28509a58d79c808180f6068
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-04 11:45:50 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- cspp1/m6/p3/digit_product.py
+++ cspp1/m6/p3/digit_product.py
@@ -3,11 +3,12 @@ I_NT = int(input())
 I_C = 1
 C_O = 1
 N_UM = 1
+if I_NT < 0:
+    N_UM = -1
+    I_NT = abs(I_NT)
+else:
+    N_UM = 1
 if I_NT != 0:
-    if I_NT < 0:
-        N_UM = -1
-    else:
-        N_UM = 1
     while I_NT >= 1:
         C_O = I_NT % 10
         I_C = I_C * C_O
