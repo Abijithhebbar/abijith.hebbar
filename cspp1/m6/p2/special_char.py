6bc9a3ed2e428cd88ef772c14a6ad37db6b00361
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-04 10:18:40 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- cspp1/m6/p2/special_char.py
+++ cspp1/m6/p2/special_char.py
@@ -6,7 +6,7 @@ Output has three spaces, which are to be replaced with these special characters
 S_C = input()
 S_CN = ""
 for i in S_C:
-    if i in '!@#$%^&*()_+-=.,:;?/':
+    if i in '!@#$%^&*()_+-=?':
         S_CN += " "
     else:
         S_CN += i
