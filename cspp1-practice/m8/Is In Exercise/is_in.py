ed0f2541fe14983074dacc04e28d688a44713b98
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-07 17:47:55 +0530

    is_in.py

--- cspp1-practice/m8/Is In Exercise/is_in.py
+++ cspp1-practice/m8/Is In Exercise/is_in.py
@@ -1,30 +1,25 @@
-# Exercise: Is In
-# Write a Python function, isIn(char, aStr), that takes in two arguments a character and String and returns the isIn(char, aStr) which retuns a boolean value.
-
-# This function takes in two arguments character and String and returns one boolean value.
-
-def isIn(char, a_str):
+"""# Exercise: Is In
+# Write a Python function, isIn(char, aStr),
+ that takes in two arguments a character and String and
+  returns the isIn(char, aStr) which retuns a boolean value.
+# This function takes in two arguments character
+ and String and returns one boolean value.
+"""
+def is_in(char_in, a_str):
     '''
     char: a single character
     aStr: an alphabetized string
-    
     returns: True if char is in aStr; False otherwise
     '''
-    b=0
-    c=0
     for i in range(len(a_str)+1):
-        if char in a_str:
-            b += 1
-    if b >=1:
-        return True
-    else:
-        return False
+        if char_in in a_str:
+            return True
 
+        return False
 def main():
+    """ main """
     data = input()
     data = data.split()
-    print(isIn((data[0][0]), data[1]))
-
-
-if __name__== "__main__":
+    print(is_in((data[0][0]), data[1]))
+if __name__ == "__main__":
     main()
