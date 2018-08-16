a700c7df902f4a5c1b1defb05d06660c903726f1
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-16 12:04:27 +0530

    " -> 15 of 19 passed. pylint: 7.98/10 "

--- cspp1/m16/CodeCampPoker/poker.py
+++ cspp1/m16/CodeCampPoker/poker.py
@@ -35,12 +35,13 @@ def is_flush(hand):
         Think of an algorithm: given the card suite how to check if it is a flush
         Write the code for it and return True if it is a flush else return False
     '''
-    temp  = [s for c,s in hand]
-    if len(set(temp)) == 1:
-        return True
-    return False
-
- def four_of_a_kind(hand):
+    temp  = hand[0]
+    for i in hand:
+        if temp[1] != i[1]:
+            return False
+    return True
+    
+def four_of_a_kind(hand):
     '''four of a kind'''
     li = []
     for h in hand:
