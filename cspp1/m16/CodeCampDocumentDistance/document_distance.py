c65934de7dba7dc3d9be52b8db3929e2190ed759
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-17 12:11:43 +0530

    " -> 2 of 3 passed. pylint: 8.95/10 "

--- cspp1/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1/m16/CodeCampDocumentDistance/document_distance.py
@@ -2,7 +2,7 @@
     Document Distance - A detailed description is given in the PDF
 '''
 import math
-file_name = "stopwords.txt"
+FILE_NAME = "stopwords.txt"
 def similarity(dict_1, dict_2):
     '''
         Compute the document distance as given in the PDF
@@ -10,24 +10,24 @@ def similarity(dict_1, dict_2):
     list_1 = ""
     list_2 = ""
     for _ in dict_1:
-    	if _ not in '!@#$%^&*()_+-=?':
-    		list_1 += _
+        if _ not in '!@#$%^&*()_+-=?':
+            list_1 += _
     for _ in dict_2:
-    	if _ not in '!@#$%^&*()_+-=?':
-    		list_2 += _
+        if _ not in '!@#$%^&*()_+-=?':
+            list_2 += _
     list_1 = list_1.split()
     list_2 = list_2.split()
     list_5 = list_1 +list_2
     adict = {}
     for word in list_5:
-    	if word not in load_stopwords(file_name).keys():
-    		adict[word] = (list_1.count(word), list_2.count(word))
+        if word not in load_stopwords(FILE_NAME).keys():
+            adict[word] = (list_1.count(word), list_2.count(word))
     numerator, add_1, add_2 = 0, 0, 0
     for a_check in adict:
-    	numerator += adict[a_check][0]*adict[a_check][1]
-    	add_1 += adict[a_check][0] ** 2
-    	add_2 += adict[a_check][1] ** 2
-    	denominator = math.sqrt(add_1) * math.sqrt(add_2)
+        numerator += adict[a_check][0]*adict[a_check][1]
+        add_1 += adict[a_check][0] ** 2
+        add_2 += adict[a_check][1] ** 2
+        denominator = math.sqrt(add_1) * math.sqrt(add_2)
     value_check = numerator/denominator
     return value_check
    
