c3495308011b43f7d8ed6ce995628cd7933f6d94
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-23 18:17:23 +0530

    " -> 6 of 8 passed. pylint: 9.82/10 "

--- cspp1/m20/matrix_operations.py
+++ cspp1/m20/matrix_operations.py
@@ -1,3 +1,6 @@
+"""
+defining the matrix multiplication and the matrix addition.
+"""
 def mult_matrix(matrix_1, matrix_2):
     '''
         check if the matrix1 columns = matrix2 rows
@@ -6,20 +9,28 @@ def mult_matrix(matrix_1, matrix_2):
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
-    rows_matrix_1 = len(matrix_1)
-    columns_matrix_1 = len(matrix_1[0])
-    rows_matrix_2 = len(matrix_2)
-    columns_matrix_2 = len(matrix_2[0])
-    result_mul = [[0 for j in range(len(matrix_1))]for i in range(len(matrix_1[0]))]
-    if columns_matrix_1 == rows_matrix_2:
-        for i in range(columns_matrix_1):
-            for j in range(rows_matrix_2):
-                for k in range(rows_matrix_2):
-                    result_mul[i][j] += matrix_1[i][k] * matrix_2[k][j]
-        return result_mul
-    else:
+    if len(matrix_1[0]) != len(matrix_2):
         print("Error: Matrix shapes invalid for mult")
         return None
+    # else:
+    res_mul = []
+    b_str = []
+    # rows = len(matrix_1)
+    cols = len(matrix_2[0])
+    rows_2 = len(matrix_2)
+    for i, value in enumerate(matrix_1):
+        # print(i, value)
+        for j in range(cols):
+            sum_value = 0
+            for k in range(rows_2):
+                sum_value += (matrix_1[i][k] * matrix_2[k][j])
+                k += 1
+            b_str.append(sum_value)
+            j += 1
+        res_mul.append(b_str)
+        i += 1
+        b_str = []
+    return res_mul
 
 def add_matrix(matrix_1, matrix_2):
     '''
@@ -29,19 +40,20 @@ def add_matrix(matrix_1, matrix_2):
         and return None
         error message should be "Error: Matrix shapes invalid for addition"
     '''
-    rows_matrix_1 = len(matrix_1)
-    columns_matrix_1 = len(matrix_1[0])
-    rows_matrix_2 = len(matrix_2)
-    columns_matrix_2 = len(matrix_2[0])
-    result_add = [[0 for j in range(len(matrix_1))]for i in range(len(matrix_1[0]))]
-    if rows_matrix_1 == rows_matrix_2 and columns_matrix_1 == columns_matrix_2:
-        for i in range(rows_matrix_1):
-            for j in range(columns_matrix_2):
-                result_add[i][j] = matrix_1[i][j] + matrix_2[i][j]
-        return result_add
-    else:
+    if len(matrix_1) != len(matrix_2) and len(matrix_1[0]) != len(matrix_2[0]):
         print("Error: Matrix shapes invalid for addition")
         return None
+    # else:
+    add = []
+    rows = len(matrix_1)
+    for i in range(rows):
+        row = []
+        for j in range(len(matrix_2[0])):
+            row.append(matrix_1[i][j] + matrix_2[i][j])
+            j += 1
+        add.append(row)
+        i += 1
+    return add
 
 def read_matrix():
     '''
@@ -51,36 +63,42 @@ def read_matrix():
         print an error message and return None
         error message should be "Error: Invalid input for the matrix"
     '''
-    rows_columns = []
-    for i in range(1):
-        rows_columns += input().split(',')
-    matrix_ = []
-    rows_ = int(rows_columns[0])
-    columns_ = int(rows_columns[1])
-    for i in range(rows_):
-        matrix_.append(list(map(int, input().rstrip().split())))
-    # print(len(matrix_))
-    # print(len(matrix_[0]))
-    # print(rows_columns[1])
-    # print(rows_columns[0])
-    if len(matrix_) == rows_columns[0] and len(matrix_[0]) == rows_columns[1]:
-        print("Error: Invalid input for the matrix")
-        return None
-    else:
-        return matrix_
+    line = input().split(',')
+    rows = int(line[0])
+    columns = int(line[1])
+    matrix = []
+    for _ in range(rows):
+        line = input().split(" ")
+        if len(line) == columns:
+            matrix.append([int(j) for j in line])
+        else:
+            print("Error: Invalid input for the matrix")
+            return None
+    return matrix
+
 def main():
+    """
+    calling the funtions and printing the output.
+    """
     # read matrix 1
+    matrix_1 = read_matrix()
 
     # read matrix 2
+    matrix_2 = read_matrix()
+
+    # print(matrix_1)
+    # print(matrix_2)
 
     # add matrix 1 and matrix 2
+    # matrix_sum = add_matrix(matrix_1, matrix_2)
+    if (matrix_1 and matrix_2):
+        sum_matrix = add_matrix(matrix_1, matrix_2)
+        print(sum_matrix)
+        print(mult_matrix(matrix_1, matrix_2))
+        # print(mult_mat)
 
     # multiply matrix 1 and matrix 2
-    matrix_1 = read_matrix()
-    matrix_2 = read_matrix()
-    print(add_matrix(matrix_1, matrix_2))
-    print(mult_matrix(matrix_1, matrix_2))
-
+    # matrix_mul = mult_matrix(matrix_1, matrix_2)
 
 if __name__ == '__main__':
     main()
