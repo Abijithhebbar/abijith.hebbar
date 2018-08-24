397f39ccb70e15c01aa6fdbd4b19c8540d2f2309
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-25 00:16:05 +0530

    " -> 0 of 14 passed. pylint: -1.67/10 "

--- cspp1/m21/tiktaktoe.py
+++ cspp1/m21/tiktaktoe.py
@@ -1,117 +1,47 @@
-"""tictactoe game for 2 players
-"""
-from __future__ import print_function
-
-
-
-choices = []
-
-
-
-for x in range (0, 9) :
-
-    choices.append(str(x + 1))
-
-
-
-playerOneTurn = True
-
-winner = False
-
-
-
-def printBoard() :
-
-    print( '\n -----')
-
-    print( '|' + choices[0] + '|' + choices[1] + '|' + choices[2] + '|')
-
-    print( ' -----')
-
-    print( '|' + choices[3] + '|' + choices[4] + '|' + choices[5] + '|')
-
-    print( ' -----')
-
-    print( '|' + choices[6] + '|' + choices[7] + '|' + choices[8] + '|')
-
-    print( ' -----\n')
-
-
-
-while not winner :
-
-    printBoard()
-
-
-
-    if playerOneTurn :
-
-        print( "Player 1:")
-
-    else :
-
-        print( "Player 2:")
-
-
-
-    try:
-
-        choice = int(input(">> "))
-
-    except:
-
-        print("please enter a valid field")
-
-        continue
-
-    if choices[choice - 1] == 'X' or choices [choice-1] == 'O':
-
-        print("illegal move, plase try again")
-
-        continue
-
-
-
-    if playerOneTurn :
-
-        choices[choice - 1] = 'X'
-
-    else :
-
-        choices[choice - 1] = 'O'
-
-
-
-    playerOneTurn = not playerOneTurn
-
-
-
-    for x in range (0, 3) :
-
-        y = x * 3
-
-        if (choices[y] == choices[(y + 1)] and choices[y] == choices[(y + 2)]) :
-
-            winner = True
-
-            printBoard()
-
-        if (choices[x] == choices[(x + 3)] and choices[x] == choices[(x + 6)]) :
-
-            winner = True
-
-            printBoard()
-
-
-
-    if((choices[0] == choices[4] and choices[0] == choices[8]) or 
-
-       (choices[2] == choices[4] and choices[4] == choices[6])) :
-
-        winner = True
-
-        printBoard()
-
-
-
-print ("Player " + str(int(playerOneTurn + 1)) + " wins!\n")
+'''Tic_Tac_Toe'''
+def tic_tac_toe(matrix):
+   '''Tic_Tac_Toe'''
+   n = len(matrix[0])
+   winner = []
+   for row in matrix:
+       if len(set(row)) == 1:
+           winner.append(row[0])
+   for i in range(0, n):
+       lis1 = []
+       for j in range(0, n):
+           lis1.append(matrix[j][i])
+       if len(set(lis1)) == 1:
+           winner.append(matrix[j][i])
+   lis2 = []
+   for i in range(0, n):
+       lis2.append(matrix[i][i])
+   if len(set(lis2)) == 1:
+       winner.append(matrix[i][i])
+   lis3 = []
+   for i in range(0, n):
+       lis3.append(matrix[(n-1)-i][i])
+   if len(set(lis3)) == 1:
+       winner.append(matrix[i][i])
+   winner = list(set(winner))
+   if winner == []:
+       print('draw')
+       return None
+   if len(winner) == 1:
+       if winner[0] == 'x' or winner[0] == 'o':
+           print(winner[0])
+       else:
+           print("invalid input")
+       return winner[0]
+   else:
+       print("invalid game")
+       return None
+def main():
+   '''main'''
+   n = int(input())
+   matrix = []
+   for _ in range(0,n):
+       col = input().split(' ')
+       matrix.append(col)
+   tic_tac_toe(matrix)
+if __name__ == '__main__':
+   main()
