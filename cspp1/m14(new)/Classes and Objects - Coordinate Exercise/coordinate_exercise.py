02f6fc468b33e5f623a2e607d20a311238afb712
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-24 21:30:46 +0530

    coordinate_exercise.py

--- cspp1/m14(new)/Classes and Objects - Coordinate Exercise/coordinate_exercise.py
+++ cspp1/m14(new)/Classes and Objects - Coordinate Exercise/coordinate_exercise.py
@@ -1,39 +1,36 @@
-# Exercise: coordinate
-
-# Consider the following code from the last lecture video:
-
+"""# Exercise: coordinate
+# Consider the following code from the last lecture video:"""
 class Coordinate(object):
-     def __init__(self, x, y):
-         self.x = x
-         self.y = y
-
-     def getX(self):
-        # Getter method for a Coordinate object's x coordinate.
-        # Getter methods are better practice than just accessing an attribute directly
-        return self.x
-
-	def getY(self):
-		# Getter method for a Coordinate object's y coordinate
-		return self.y
-
-        
-	def __str__(self):
-		return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
-
-# Your task is to define the following two methods for the Coordinate class:
-# Add an __eq__ method that returns True if coordinates refer to same point in the plane (i.e., have the same x and y coordinate).
-# Define __repr__, a special method that returns a string that looks like a valid Python expression that could be used to recreate an
-# object with the same value. In other words, eval(repr(c)) == c given the definition of __eq__ from part 1.
-
-
-
-
+    """ coordinate function"""
+    def __init__(self, inp_x, inp_y):
+        """intialize"""
+        self.inp_x = inp_x
+        self.inp_y = inp_y
+    def get_x(self):
+        """ Getter method for a Coordinate object's x coordinate.
+        # Getter methods are better practice than just accessing an attribute directly"""
+        return self.inp_x
+    def get_y(self):
+        """ Getter method for a Coordinate object's y coordinate"""
+        return self.inp_y
+    def __str__(self):
+        return '<' + str(self.get_x()) + ',' + str(self.get_y()) + '>'
+    def __eq__(self, other):
+        if self.get_x() == other.get_x():
+            if self.get_y() == other.get_y():
+                return True
+        return False
+    def __repr__(self):
+        return 'Coordinate(' + str(self.get_x()) + ',' + str(self.get_y()) + ')'
+    # object with the same value. In other words,
+    # eval(repr(c)) == c given the definition of __eq__ from part 1.
 def main():
-	data = raw_input()
-	data = data.split(' ')
-	data = list(map(int, data))
-	print(Coordinate(data[0],data[1]) == Coordinate(data[2],data[3]))
-	print(Coordinate(data[4],data[5]).__repr__())
+    """main """
+    data = input()
+    data = data.split(' ')
+    data = list(map(int, data))
+    print(Coordinate(data[0], data[1]) == Coordinate(data[2], data[3]))
+    print(Coordinate(data[4], data[5]).__repr__())
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
