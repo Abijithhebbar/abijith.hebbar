6502fbcfe3a29757d02c869809b88932fed36295
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-25 12:50:04 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- cspp1/m22 exam/assignment5/frequency_graph.py
+++ cspp1/m22 exam/assignment5/frequency_graph.py
@@ -4,11 +4,43 @@ frequency of each word. Display the frequency values using “#” as a text bas
 '''
 
 def frequency_graph(dictionary):
-	x = sorted(dictionary.keys())
-	for i in x:
-		print(x)
+	"""frequency function"""
+	sorted_dict = sorted(dictionary.keys())
+	for i in sorted_dict:
+		if dictionary[i] == 1:
+			j="#"
+		if dictionary[i] == 2:
+			j="##"
+		if dictionary[i] == 3:
+			j="###"
+		if dictionary[i] == 4:
+			j="####"
+		if dictionary[i] == 5:
+			j="#####"
+		if dictionary[i] == 6:
+			j="######"
+		if dictionary[i] == 7:
+			j="#######"
+		if dictionary[i] == 8:
+			j="########"
+		if dictionary[i] == 9:
+			j="#########"
+		if dictionary[i] == 10:
+			j="##########"
+		if dictionary[i] == 11:
+			j="###########"
+		if dictionary[i] == 12:
+			j="############"
+		if dictionary[i] == 13:
+			j="#############"
+		if dictionary[i] == 14:
+			j="##############"
+		if dictionary[i] == 12:
+			j="###############"
+		print(i, '-', j)
 
 def main():
+	"""main function"""
     dictionary = eval(input())
     frequency_graph(dictionary)
 
