734705b7b42b6b4842837aa49b927d75bafa0007
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-25 12:35:12 +0530

    " -> 3 of 3 passed. pylint: 8.89/10 "

--- cspp1/m22 exam/assignment4/print_dictionary.py
+++ cspp1/m22 exam/assignment4/print_dictionary.py
@@ -11,7 +11,7 @@ def print_dictionary(dictionary):
         print(i, '-', dictionary[i])
 def main():
     """main function"""
-    dictionary = input()
+    dictionary = eval(input())
     print_dictionary(dictionary)
 
 if __name__ == '__main__':
