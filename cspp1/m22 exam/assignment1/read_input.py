50acb259ed74f6777ad222682c8d2535e81b9a00
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-25 10:54:05 +0530

    " -> 2 of 2 passed. pylint: 6.25/10 "

--- cspp1/m22 exam/assignment1/read_input.py
+++ cspp1/m22 exam/assignment1/read_input.py
@@ -3,7 +3,12 @@ Write a python program to read multiple lines of text input and store the input 
 '''
 
 def main():
-    pass
+    number_of_lines = int(input())
+    string_input = ""
+    for i in range(number_of_lines):
+    	string_input += input() + "\n"
+    print(string_input)
+
 
 if __name__ == '__main__':
     main()
