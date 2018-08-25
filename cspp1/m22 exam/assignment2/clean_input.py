43c577b5fe587c8b116f67dae9e6a37e6a5d2a02
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-25 10:45:59 +0530

    " -> 2 of 3 passed. pylint: 10.00/10 "

--- cspp1/m22 exam/assignment2/clean_input.py
+++ cspp1/m22 exam/assignment2/clean_input.py
@@ -1,16 +1,17 @@
 '''
-Write a function to clean up a given string by removing the special characters and retain 
+Write a function to clean up a given string by removing the special characters and retain
 alphabets in both upper and lower case and numbers.
 '''
-
 def clean_string(inp_string):
-	new_string = ""
-	for word in inp_string:
-		if word not in "`~!@#$%^&*()_+=- \''":
-			new_string += word
-	return new_string
+    """ Clean function"""
+    new_string = ""
+    for word in inp_string:
+        if word not in "`~!@#$%^&*()_+=-\''":
+            new_string += word
+    return new_string.replace(" ", "")
 
 def main():
+    """main function"""
     inp_string = input()
     print(clean_string(inp_string))
 
