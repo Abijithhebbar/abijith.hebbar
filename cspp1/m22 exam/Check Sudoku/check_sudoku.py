5507ff4f5c18102d7a46886f44f81d4f90610486
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-25 13:21:57 +0530

    " -> 0 of 26 passed. pylint: 0/0 "

--- cspp1/m22 exam/Check Sudoku/check_sudoku.py
+++ cspp1/m22 exam/Check Sudoku/check_sudoku.py
@@ -16,14 +16,46 @@ def check_sudoku(sudoku):
     output = True
     for i in range(0, 9, 1):
         for j in range(0, 9, 1):
-            if j<8:
+            if j < 8:
                 if sudoku[i][j] == sudoku[i][j+1]:
                     output = False
     for i in range(0, 9, 1):
         for j in range(0, 9, 1):
-            if j<8:
+            if j < 8:
                 if sudoku[j][i] == sudoku[j+1][i]:
                     output = False
+    for i in range(0, 3, 1):
+        for j in range(0, 3, 1):
+            if j < 2:
+                if sudoku[i][j] == sudoku[i][j+1]:
+                    output = False
+    for i in range(0, 3, 1):
+        for j in range(0, 3, 1):
+            if j < 3:
+                if sudoku[j][i] == sudoku[j+1][i]:
+                    output False
+    for i in range(3, 6, 1):
+        for j in range(3, 6, 1):
+            if j < 5:
+                if sudoku[i][j] == sudoku[i][j+1]:
+                    output = False
+    for i in range(3, 6, 1):
+        for j in range(3, 6, 1):
+            if j < 5:
+                if sudoku[j][i] == sudoku[j+1][i]:
+                    output False
+    for i in range(6, 9, 1):
+        for j in range(6, 9, 1):
+            if j < 8:
+                if sudoku[i][j] == sudoku[i][j+1]:
+                    output = False
+    for i in range(6, 9, 1):
+        for j in range(6, 9, 1):
+            if j < 8:
+                if sudoku[j][i] == sudoku[j+1][i]:
+                    output False
+
+
     return output
 
 
@@ -32,12 +64,11 @@ def main():
         main function to read input sudoku from console
         call check_sudoku function and print the result to console
     '''
-    
     # initialize empty list
     sudoku = []
 
     # loop to read 9 lines of input from console
-    for i in range(9):
+    for _ in range(9):
         # read a line, split it on SPACE and append row to list
         row = input().split(' ')
         sudoku.append(row)
