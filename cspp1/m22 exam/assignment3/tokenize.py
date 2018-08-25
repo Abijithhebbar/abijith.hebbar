f03985dbda9fc7c2d0b15261f848a22200100261
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-25 11:52:28 +0530

    " -> 3 of 4 passed. pylint: 7.78/10 "

---
+++ cspp1/m22 exam/assignment3/tokenize.py
@@ -0,0 +1,32 @@
+'''
+Write a function to tokenize a given string and return a dictionary with the frequency of
+each word
+'''
+import re
+# output_dictionary = {}
+def tokenize(string_input):
+    output_dictionary = {}
+    list_input = string_input.split()
+    for word in list_input:
+        if word in output_dictionary:
+            output_dictionary[word] += 1
+        else:
+            output_dictionary.update({word:1})
+    return output_dictionary
+    # for key in list_input:
+    #     key = re.sub('[^a-zA-z0-9]', '', key)
+    #     if key not in output_dictionary:
+    #         output_dictionary[key] = 1
+    #     else:
+    #         output_dictionary[key] += 1
+            
+def main():
+    number_of_lines = int(input())
+    string_input = ""
+    for _ in range(0, number_of_lines, 1):
+        string_input += input()
+        tokenize(string_input)
+    print(tokenize(string_input))
+
+if __name__ == '__main__':
+    main()
